af8dee57f43bbc1cd06b1dd41ae0d40bf7d2b1e2
gozdesimsekk <gozdesimsekk1@gmail.com>
2024-09-27 15:44:06 +0300

    new_tuseb_project

--- src/Sections/Research/index.js
+++ src/Sections/Research/index.js
@@ -4,7 +4,6 @@ import styled,{ css } from 'styled-components';
 
 // Define styled components
 const Container = styled.div`
- 
   display:flex;
   flex-direction: column;
   max-width: 80%;
@@ -33,7 +32,6 @@ const ResearchTitle = styled.h2`
   font-weight:600
 `;
 
-
 const ResearchBody = styled.p`
   margin: 0;
 
@@ -45,13 +43,21 @@ const ResearchBody = styled.p`
 
 const ResearchList = () => {
   return (
-    <Container  id="research">
+    <Container id="research">
       <Title>Our Projects</Title>
       <ResearchItem>
+        <ResearchTitle>The single-cell transcriptomic analysis of myeloid-derived suppressor cells in lung cancer</ResearchTitle>
+        <ResearchBody>
+        Turkish National Institute of Health (TÜSEB) 2024-B01 <br/>
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz  <br/>
+          Contributors: Prof. Dr. Güneş Esendağlı, Prof. Dr. Tuncay Göksel, Prof. Dr. Ayşe Caner, Dr. Haydar Soydaner Karakuş, Dr. Ufuk Mert, PhD Candidate Ebru Kocakaya
+        </ResearchBody>
+      </ResearchItem>
+      <ResearchItem>
         <ResearchTitle>Extension of endSeqTools with single-cell RNAseq data</ResearchTitle>
-        <ResearchBody >
+        <ResearchBody>
         Scientific and Technological Research Council of Turkey (TÜBİTAK) 1002 <br/>
-          Researchers: Yasin Kaymaz <br/>
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz <br/>
           Contributors: Amir Amiri Tabat, Melike Güler, Elyar Bafandeh Zendeh
         </ResearchBody>
       </ResearchItem>
@@ -59,23 +65,23 @@ const ResearchList = () => {
         <ResearchTitle>Enrichment of Alternative Target Antigen Pool for Car T Cell Therapy Against Non-Small Cell Lung Cancer with Single-Cell Genomic Data</ResearchTitle>
         <ResearchBody>
         Scientific and Technological Research Council of Turkey (TÜBİTAK) 3501 <br/>
-          Researchers: Yasin Kaymaz, Ayşe Caner, Ali Veral, Kutsal Turhan, Tevfik İlker Akçam, Hasan Bulut <br/>
-          Contributors: Ebru Kocakaya, Duygu Keremitçi, Ozan Göçmen, Beyza Aygün
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz, Prof. Dr. Ayşe Caner, Prof. Dr. Ali Veral, Prof. Dr. Kutsal Turhan, Assoc. Prof. Dr. Tevfik İlker Akçam, Prof. Dr. Hasan Bulut <br/>
+          Contributors: PhD Candidate Ebru Kocakaya, Duygu Keremitçi, Ozan Göçmen, Beyza Aygün
         </ResearchBody>
       </ResearchItem>
       <ResearchItem>
         <ResearchTitle>Investigation of Biomarker Features of Alternative Transcript Isoforms in Tumor-Associated Macrophage Plasticity Using Single-Cell RNA Sequencing Approach</ResearchTitle>
         <ResearchBody>
         Turkish National Institute of Health (TÜSEB) 2022-B-01 <br/>
-          Researchers: Yasin Kaymaz, Hilal Kazan, Güneş Esendağlı, Ayşe Caner <br/>
-          Contributors: Ebru Kocakaya, Hamdullah, Ece
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz, Prof. Dr. Hilal Kazan, Prof. Dr. Güneş Esendağlı, Prof. Dr. Ayşe Caner <br/>
+          Contributors: PhD Candidate Ebru Kocakaya, Hamdullah, Ece
         </ResearchBody>
       </ResearchItem>
       <ResearchItem>
         <ResearchTitle>Development of Expert System for Personalized Bioinformatics Analysis</ResearchTitle>
         <ResearchBody>
         Scientific and Technological Research Council of Turkey (TÜBİTAK) TEYDEB <br/>
-          Researchers: Yasin Kaymaz, Talha Güler, Hasan Bulut <br/>
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz, Talha Güler, Hasan Bulut <br/>
           Contributors: Amir Amiri Tabat, Gözde Şimşek, Elif Duymaz, Burak Aydınoğlu
         </ResearchBody>
       </ResearchItem>
@@ -83,30 +89,30 @@ const ResearchList = () => {
         <ResearchTitle>Development of Analysis Pathway for Transcriptome Profiling of Gastroesophageal Reflux Disease Phenotypes with RNA Sequencing</ResearchTitle>
         <ResearchBody>
           EGE UNIVERSITY - ALİYE ÜSTER FOUNDATION <br/>
-          Researchers: Yasin Kaymaz, Pelin Ergün, Sezgi Kıpçak
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz, Postdoc Pelin Ergün, PhD Sezgi Kıpçak
         </ResearchBody>
       </ResearchItem>
       <ResearchItem>
         <ResearchTitle>Development of a Bioinformatics Approach for Determining Functional Subgroups Based on Gene Set Scoring in Single-Cell Transcriptomics Data</ResearchTitle>
         <ResearchBody>
         Scientific and Technological Research Council of Turkey (TÜBİTAK) COST 2515 <br/>
-          Researchers: Yasin Kaymaz <br/>
-          Contributors: Ebru Kocakaya, Zeynep Garan
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz <br/>
+          Contributors: PhD Candidate Ebru Kocakaya, Zeynep Garan
         </ResearchBody>
       </ResearchItem>
       <ResearchItem>
         <ResearchTitle>Development of a Machine Learning Model for Classifying Tumor-Associated Macrophage Cells into Subgroups</ResearchTitle>
         <ResearchBody>
-        Turkish National Institute of Health (TÜSEB)  A2-YL <br/>
-          Researchers: Duygu Keremitçi, Yasin Kaymaz <br/>
-          Contributors: Ebru Kocakaya, Hamdullah, Ece, Güneş Esendağlı
+        Turkish National Institute of Health (TÜSEB) A2-YL <br/>
+          Researchers: Asst. Prof. Dr. Yasin Kaymaz, PhD Candidate Duygu Keremitçi <br/>
+          Contributors: Prof. Dr. Güneş Esendağlı, PhD Candidate Ebru Kocakaya, Hamdullah, Ece, 
         </ResearchBody>
       </ResearchItem>
       <ResearchItem>
         <ResearchTitle>Rat Brain Atlas Project- Single cell survey of Rat brain</ResearchTitle>
         <ResearchBody>
           Researchers: Boehringer Ingelheim <br/>
-          Contributors: Yasin Kaymaz, Nathan Lawless, Kelly Allers
+          Contributors: Asst. Prof. Dr. Yasin Kaymaz, Nathan Lawless, Kelly Allers
         </ResearchBody>
       </ResearchItem>
     </Container>
