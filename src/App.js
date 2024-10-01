816a7453c2025d724d4f85fc76913e055bb0f129
gozdesimsekk <gozdesimsekk1@gmail.com>
2024-10-01 15:26:18 +0300

    mix-panel_added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { GlobalStyle } from "./globalStyles";
-import { lazy, Suspense } from "react";
-
+import { lazy, Suspense, useEffect } from "react";
+import mixpanel from './mixpanel'; 
 const Home = lazy(() => import("./Pages/Home"));
 const Header = lazy(() => import("./components/Header/index"));
 const Footer = lazy(() => import("./components/Footer/index"));
 const ScrollToTop = lazy(() => import("./components/ScrollToTop/index"));
 
 function App() {
+  useEffect(() => {
+    mixpanel.track('bmglab Page View', {
+      page: 'Home' 
+    });
+  }, []);
   return (
     <>
       <Suspense fallback={null}>
---
+++ src/mixpanel.js
@@ -0,0 +1,5 @@
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('4cd697e8ca7470752857c1243fff22dc'); 
+
+export default mixpanel;
